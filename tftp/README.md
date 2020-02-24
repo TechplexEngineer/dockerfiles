b6c380b8716059b385f24ed1072352998abd5f1d
Blake Bourque <techplex.engineer@gmail.com>
2020-02-24 12:04:23 -0500

    Add docs

--- tftp/README.md
+++ tftp/README.md
@@ -3,13 +3,13 @@
 To build
 
 ```
-docker build --rm --tag=tftp .
+docker build --rm --tag=$USER/tftp .
 ```
 
 To run
 
 ```
-docker run -p 0.0.0.0:69:69/udp -i -t tftp
+docker run -p 0.0.0.0:69:69/udp -i -t $USER/tftp
 ```
 
 Mounts the following volume for persistent data
@@ -25,5 +25,6 @@ docker run \
     -p 0.0.0.0:69:69/udp \
     -v /var/tftpboot:/var/tftpboot \
     -i \
-    -t tftp
+    -t \
+    $USER/tftp
 ```
--- tftp/tftp.sh
+++ tftp/tftp.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# ==============================================================================
+# Use this script to easily start a tftp server container.
+# When no args are passed, the current directory will be served by the tftp server
+# Optionally pass a path to serve as the first argument.
+# ==============================================================================
+
 mountPath="$1"
 
 # if length of $1 == 0
@@ -8,8 +14,12 @@ if [ -z "$1" ]; then
     mountPath="$(pwd)"
 fi
 
+echo "Starting TFTP server at ${mountPath}"
+echo "Type CTRL-C to stop"
+
 docker run \
     -p 0.0.0.0:69:69/udp \
     -v "${mountPath}:/var/tftpboot" \
     -i \
-    -t tftp
+    -t \
+    "$USER/tftp"
